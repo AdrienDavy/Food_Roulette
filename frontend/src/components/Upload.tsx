87d0948702212fa8149a0a34861068656728705a
Adrien Davy <adriendavyfx@gmail.com>
2025-01-09 21:49:20 +0100

    Ajout de la gestion de la progression et des notifications d'erreur lors du chargement d'images ; amélioration de l'interface utilisateur pour le composant Upload.

--- frontend/src/components/Upload.tsx
+++ frontend/src/components/Upload.tsx
@@ -4,7 +4,8 @@ import {
   publicKey,
   urlEndpoint,
 } from "../api/imagekitio/ImageKitIoAuth";
-import { useRef } from "react";
+import { useRef, useState } from "react";
+import { Bounce, toast } from "react-toastify";
 
 type IKUploadType = React.ComponentProps<typeof IKUpload>;
 
@@ -23,22 +24,119 @@ interface UploadProps {
 
 const Upload: React.FC<UploadProps> = (props) => {
   const ikUploadRef = useRef<HTMLInputElement>(null);
+  const [progress, setProgress] = useState(0);
+
+  const onError = () => {
+    const error = new Error("Upload failed");
+    toast.error(`Erreur lors du chargement de l'image: ${error.message} 🦄`, {
+      className: "toast-error bg-danger",
+      position: "top-right",
+      autoClose: 5000,
+      hideProgressBar: false,
+      closeOnClick: true,
+      pauseOnHover: true,
+      draggable: true,
+      progress: undefined,
+      theme: "light",
+      transition: Bounce,
+    });
+  };
+
+  // Gestion du succès
+  interface SuccessResponse {
+    thumbnailUrl: string;
+    name: string;
+    fileType: string;
+  }
+
+  const onSuccess: UploadProps["onSuccess"] = (res: SuccessResponse) => {
+    console.log("Success", res);
+
+    toast.success(
+      <div className="flex flex-col items-center">
+        <img
+          src={res.thumbnailUrl}
+          alt={res.name}
+          className="w-full mb-3 object-cover rounded-lg"
+        />
+        <span>
+          {res.fileType} <strong>{res.name}</strong> chargée avec succès ! 🎉
+        </span>
+      </div>,
+      {
+        className: "toast-success bg-primary",
+        position: "top-right",
+        autoClose: 5000,
+        hideProgressBar: false,
+        closeOnClick: true,
+        pauseOnHover: true,
+        draggable: true,
+        progress: undefined,
+        theme: "light",
+        transition: Bounce,
+      }
+    );
+  };
+
+  // Gestion de la progression
+  const handleProgress = (event: ProgressEvent) => {
+    if (event.lengthComputable) {
+      const percentComplete = Math.round((event.loaded / event.total) * 100);
+      setProgress(percentComplete);
+      if (percentComplete === 100) {
+        setTimeout(() => {
+          setProgress(0);
+        }, 5000);
+      }
+    }
+  };
+
   return (
-    <div>
+    <div className="upload-container relative">
       <IKContext
         publicKey={publicKey}
         urlEndpoint={urlEndpoint}
         transformationPosition="path"
         authenticator={authenticator}
       >
-        <IKUpload {...props} style={{ display: "none" }} ref={ikUploadRef} />
+        {/* Upload hidden for custom button */}
+        <IKUpload
+          {...props}
+          style={{ display: "none" }}
+          ref={ikUploadRef}
+          onError={onError}
+          onSuccess={onSuccess}
+          onUploadProgress={(event) => {
+            handleProgress(event);
+            if (props.onUploadProgress) props.onUploadProgress(event);
+          }}
+        />
 
-        <button
-          className="primary-button"
-          onClick={() => ikUploadRef.current?.click()}
-        >
-          Charger une image
-        </button>
+        <div className="relative w-fit h-14">
+          <div
+            className={`${
+              progress ? "visible opacity-100" : " hidden opacity-0"
+            } w-full z-20 absolute -top-10 `}
+          >
+            <p className="text-center py-2 animate-pulse text-light dark:text-primary-hover">
+              {progress < 100
+                ? `Chargement... ${progress}%`
+                : "Chargement terminé !"}
+            </p>
+            <div className="w-full bg-gray-200 rounded-t-md overflow-hidden h-2.5 dark:bg-gray-700">
+              <div
+                className="bg-primary dark:bg-primary-dark h-2.5 transition-all duration-300"
+                style={{ width: `${progress}%` }}
+              ></div>
+            </div>
+          </div>
+          <button
+            className="primary-button"
+            onClick={() => ikUploadRef.current?.click()}
+          >
+            Charger une image
+          </button>
+        </div>
       </IKContext>
     </div>
   );
--- frontend/src/pages/RecipeCreation.tsx
+++ frontend/src/pages/RecipeCreation.tsx
@@ -1,85 +1,15 @@
-import { Bounce, toast } from "react-toastify";
 import BrandManager from "../components/dataManagers/BrandManager";
 import IngredientManager from "../components/dataManagers/IngredientManager";
 import Image from "../components/Image";
 import Upload from "../components/Upload";
 import { handleScrollToElement } from "../utils/handleScrollToElement";
-import { useState } from "react";
 
 const Creation = () => {
   // --------------------------------STATES--------------------------------
-  const [progress, setProgress] = useState(0);
+
   // --------------------------------QUERY--------------------------------
 
   // -----------------------------FUNCTIONS-----------------------------------
-
-  const onError = () => {
-    const error = new Error("Upload failed");
-    toast.error(`Erreur lors du chargement de l'image: ${error.message} 🦄`, {
-      className: "toast-error bg-danger",
-      position: "top-right",
-      autoClose: 5000,
-      hideProgressBar: false,
-      closeOnClick: true,
-      pauseOnHover: true,
-      draggable: true,
-      progress: undefined,
-      theme: "light",
-      transition: Bounce,
-    });
-  };
-
-  interface UploadSuccessResponse {
-    thumbnailUrl: string;
-    name: string;
-    fileType: string;
-  }
-
-  const onSuccess = (res: UploadSuccessResponse) => {
-    console.log("Success", res);
-
-    toast.success(
-      <div className="flex flex-col items-center">
-        <img
-          src={res.thumbnailUrl}
-          alt={res.name}
-          className="w-full mb-3 object-cover rounded-lg"
-        />
-        <span>
-          {res.fileType} <strong>{res.name}</strong> chargée avec succès ! 🎉
-        </span>
-      </div>,
-      {
-        className: "toast-success bg-primary",
-        position: "top-right",
-        autoClose: 5000,
-        hideProgressBar: false,
-        closeOnClick: true,
-        pauseOnHover: true,
-        draggable: true,
-        progress: undefined,
-        theme: "light",
-        transition: Bounce,
-      }
-    );
-  };
-
-  interface UploadProgressEvent extends ProgressEvent {
-    lengthComputable: boolean;
-    loaded: number;
-    total: number;
-  }
-
-  const onUploadProgress = (event: UploadProgressEvent) => {
-    if (event.lengthComputable) {
-      const percentComplete = Math.round((event.loaded / event.total) * 100);
-      setProgress(percentComplete); // Met à jour le pourcentage
-      console.log("Progress:", percentComplete + "%");
-    }
-    setTimeout(() => {
-      setProgress(0);
-    }, 5000);
-  };
 
   return (
     <div>
@@ -110,33 +40,10 @@ const Creation = () => {
           loading="lazy"
           lqip={{ active: true, quality: 20 }}
         />
-        <div className="relative">
-          <div
-            className={`${
-              !progress ? "visible opacity-100" : " hidden opacity-0"
-            } w-full `}
-          >
-            <p className="text-center py-2 animate-pulse text-light dark:text-primary-hover">
-              {progress < 100
-                ? `Chargement... ${progress}%`
-                : "Chargement terminé !"}
-            </p>
-            <div className="w-full bg-gray-200 rounded-full h-2.5 dark:bg-gray-700">
-              <div
-                className="bg-primary-focus dark:bg-primary-dark-hover h-2.5 rounded-full transition-all duration-300"
-                style={{ width: `${progress}%` }}
-              ></div>
-            </div>
-          </div>
 
-          <Upload
-            useUniqueFileName
-            onError={onError}
-            onSuccess={onSuccess}
-            onUploadProgress={onUploadProgress}
-          />
-        </div>
+        <Upload useUniqueFileName />
       </div>
+
       <BrandManager />
       <IngredientManager />
     </div>
